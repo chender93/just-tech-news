a11b41e0257a19856b2581855e8b87211f075606
Curtis <chender93@gmail.com>
2020-10-28 23:29:05 -0400

    Updated security mesaures

--- models/User.js
+++ models/User.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcrypt');
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
@@ -36,35 +37,32 @@ User.init(
         },
         // define a password column
         password: {
-          type: DataTypes.STRING,
-          allowNull: false,
-          validate: {
-            // this means the password must be at least four characters long
-            len: [4]
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+              len: [4]
+            }
           }
+        },
+        {
+            hooks: {
+                // set up beforeCreate lifecycle "hook" functionality
+                async beforeCreate(newUserData) {
+                  newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                  return newUserData;
+                },
+                // set up beforeUpdate lifecycle "hook" functionality
+                async beforeUpdate(updatedUserData) {
+                  updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                  return updatedUserData;
+                }
+              },
+          sequelize,
+          timestamps: false,
+          freezeTableName: true,
+          underscored: true,
+          modelName: 'user'
         }
-      },
-      {
-        sequelize,
-        timestamps: false,
-        freezeTableName: true,
-        underscored: true,
-        modelName: 'user'
-  },
-  {
-    // TABLE CONFIGURATION OPTIONS GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration))
-
-    // pass in our imported sequelize connection (the direct connection to our database)
-    sequelize,
-    // don't automatically create createdAt/updatedAt timestamp fields
-    timestamps: false,
-    // don't pluralize name of database table
-    freezeTableName: true,
-    // use underscores instead of camel-casing (i.e. `comment_text` and not `commentText`)
-    underscored: true,
-    // make it so our model name stays lowercase in the database
-    modelName: 'user'
-  }
-);
+      );
 
 module.exports = User;
